2a49335c6de1b81b54a012b25955e06616cccefa
Glenn Moss <gim@keynetics.com>
2013-04-10 17:05:35 -0600

    FMSynth

--- device.py
+++ device.py
@@ -1,7 +1,8 @@
 from collections import deque
-from util import NamedDescriptor, NamedMeta, configable, clamp
-from math import sin, floor, pi, log10
+from math import sin, cos, floor, pi, log10
 from numbers import Number
+from util import NamedDescriptor, NamedMeta, configable, clamp
+import operator
 
 _tau = 2*pi
 
@@ -161,19 +162,53 @@ class PositiveSignal (Signal):
     return (self._input(t) + 1)/2
 
 class LinearRamp (Signal):
-  def __init__ (self, t_start, dur):
+  def __init__ (self, t_start, dur, begin=-1, end=1):
     self.t_start = t_start
     self.dur = dur
+    self.begin = begin
+    self.end = end
 
   def __call__ (self, t):
     if t < self.t_start:
-      return -1
-      #return 0
+      return self.begin
     if t > self.t_start + self.dur:
-      return 1
+      return self.end
 
-    return (t - self.t_start) / self.dur * 2 - 1
-    #return (t - self.t_start) / self.dur
+    return (t - self.t_start) / self.dur * (self.end - self.begin) + self.begin
+
+class SegmentedRamp (Signal):
+  def __init__ (self, dur, steps, low=0, high=1):
+    self.dur = dur
+    self.steps = iter(steps)
+    self.low = low
+    self.high = high
+    self.next_t = 0
+    self.next_val = 0
+    self._next()
+
+  def _next (self):
+    self.cur_t = self.next_t
+    self.cur_val = self.next_val
+
+    start_t, val = next(self.steps)
+    start_t *= self.dur
+    val = (self.high - self.low) * val + self.low
+
+    self.next_t = start_t
+    self.next_val = val
+
+  def __call__ (self, t):
+    try:
+      while self.next_t <= t:
+        self._next()
+    except StopIteration:
+      return self.cur_val
+
+    if t > self.dur:
+      return self.cur_val
+
+    return ((t - self.cur_t) / (self.next_t - self.cur_t) *
+            (self.next_val - self.cur_val) + self.cur_val)
 
 class PolyRamp (Signal):
   def __init__ (self, t_start, dur, power=2):
@@ -316,6 +351,9 @@ class PhasedSignal (Signal):
 class Sine (PhasedSignal):
   _phase = [sin(_tau*p/1024) for p in range(1024)]
 
+class Cosine (PhasedSignal):
+  _phase = [cos(_tau*p/1024) for p in range(1024)]
+
 class Saw (PhasedSignal):
   _phase = [1 - 2*p/1024 for p in range(1024)]
 
@@ -356,22 +394,45 @@ class Amp (Signal):
   def __call__ (self, t):
     return self._ratio(t) * self._input(t)
 
-def Mult (factor, carrier):
+def BinaryMod (func):
+  def Mod (mod, carrier):
+    class BinaryMod (type(carrier)):
+      left = Input()
+      right = Input()
 
-  class Mult (type(carrier)):
-    left = Input()
-    right = Input()
+      def __init__ (self, left, right):
+        self.left = left
+        self.right = right
 
-    def __init__ (self, left, right):
-      self.left = left
-      self.right = right
+      def __call__ (self, t):
+        return func(self._left(t), self._right(t))
 
-    def __call__ (self, t):
-      return self._left(t) * self._right(t)
+    return BinaryMod(mod, carrier)
 
-  return Mult(factor, carrier)
+  return Mod
 
-class Bias (Signal):
+
+
+
+Mult = BinaryMod(operator.mul)
+Bias = BinaryMod(operator.add)
+
+#def Mult (factor, carrier):
+
+  #class Mult (type(carrier)):
+    #left = Input()
+    #right = Input()
+
+    #def __init__ (self, left, right):
+      #self.left = left
+      #self.right = right
+
+    #def __call__ (self, t):
+      #return self._left(t) * self._right(t)
+
+  #return Mult(factor, carrier)
+
+class OldBias (Signal):
   input = Input()
   offset = Input()
 
@@ -429,6 +490,14 @@ def AMSynth (input, factor=2):
   modulator = Sine(Mult(factor, input))
   return Amp(modulator, carrier)
 
+@configable
+def FMSynth (input, H=1, B=1):
+  f_modulator = Mult(H, input)
+  d_carrier = Mult(B, f_modulator)
+  modulator_osc = Sine(f_modulator)
+  modulator = Mult(d_carrier, modulator_osc)
+  return Cosine(Bias(modulator, input))
+
 def RMSynth (input, freq=50):
   carrier = Sine(input)
   modulator = Sine(freq)
@@ -436,7 +505,7 @@ def RMSynth (input, freq=50):
 
 @configable
 def Vibrato (input, freq=6, cents=50):
-  modulator = Bias(1, Mult(0.0005946*cents, Sine(freq)))
+  modulator = OldBias(1, Mult(0.0005946*cents, Sine(freq)))
   return Mult(modulator, input)
 
 def Mixer (synths):
@@ -529,23 +598,33 @@ if __name__ == '__main__':
   #rw = random_walk()
   #synth = Synth(modifier=Vibrato(freq=3.2), oscillator=Square, A=0.13, D=0.03, S=0.5, R=0.5,
   #synth = Synth(oscillator=FourierSaw(20), A=0.03, D=0.03, S=5, R=0.5,
-  synth = Synth(oscillator=Saw, A=0.03, D=0.03, S=5, R=0.5,
+  #synth = Synth(oscillator=Saw, A=0.03, D=0.03, S=5, R=0.5,
   #synth = Synth(oscillator=FourierTriangle(80), A=0.03, D=0.03, S=5, R=0.5,
   #synth = Synth(oscillator=Sine, A=0.05, D=0.03, S=5, R=0.5,
                 #modifier=Vibrato(freq=4, cents=25),
-    steps = (
-    (440, 0.25),
-    (440 * 2**(2/12), 0.25),
-    (440 * 2**(3/12), 0.25),
-    (None, 1.25),
-    (220, 0.25),
-    (220 * 2**(2/12), 0.25),
-    (220 * 2**(3/12), 0.25),
-  ))
-  play(synth, 4)
+  #synth = Synth(oscillator=FMSynth(B=5, H=1), A=0.03, D=0.03, S=1, R=0.5,
+    #steps = (
+    #(440, 0.25),
+    #(440 * 2**(2/12), 0.25),
+    #(440 * 2**(3/12), 0.25),
+    #(None, 1.25),
+    #(220, 0.25),
+    #(220 * 2**(2/12), 0.25),
+    #(220 * 2**(3/12), 0.25),
+  #))
+  #play(synth, 4)
   #play(Amp(Mult(-1, FourierSaw(20)(4)), synth), 4)
   #play(Amp(Mult(-1, Saw(4)), synth), 4)
 
+  #synth = Synth(oscillator=FMSynth(B=LinearRamp(0,2,0,10), H=0.1),
+  envelope = SegmentedRamp(2,
+     #steps=((0.01, 1), (0.4, 0.7), (0.9, 0.9), (1, 0))); H=1;B=5 # Brassy?
+     #steps=((0.06, 0.5), (0.1, 1), (0.9, 1), (1, 0))); H=1/3; B=2 # Woodwind?
+     steps=((0.06, 0.5), (0.1, 1), (0.9, 1), (1, 0))); H=0.2; B=1.5 # Bassoon?
+     #steps=((0.1, 1), (0.75, 1), (1, 0))); H=2/3; B=2 # Clarinet?
+  oscillator = FMSynth(ConstFrequency(400), B=Mult(B, envelope), H=H)
+  synth = Mult(envelope, oscillator)
+  play(synth, 2)
 
     #steps=iter([next(rw) for x in range(40)] + [(None, 0.5)]))
   #write(synth, 10.5)
